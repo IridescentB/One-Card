31f66dfc7f64112c0e5c9445b36eab0e4b0fa84a
Admin <Admin@Admin-BB8>
2017-05-29 12:51:24 +0900

    2017.05.29 Mon 12:51
    Class Gamee : added new functions

--- OneCard/src/P/Gamee.java
+++ OneCard/src/P/Gamee.java
@@ -3,13 +3,21 @@ import java.util.Random;
 
 public class Gamee {
 	int nMainCard = 0;
+	EachCard[] iCard = new EachCard[52];
 	String[] info = new String[2];
 	int infoIndex;
 	Deckk closedDeck = new Deckk();
 	Deckk openDeck = new Deckk();
 	int nGet;
+	int nThrow;
 	Handd p1Hand = new Handd();
 	Handd p2Hand = new Handd();	
+	
+	Gamee(){
+		for(int i = 0; i<52; i++) {
+			iCard[i] = new EachCard(i);
+		}
+	}
 	
 	int randCard() {
 		Random random = new Random();
@@ -27,12 +35,11 @@ public class Gamee {
 		while(true) {
 			nGet = this.randCard(); 
 			for(int i = 0; i < closedDeck.nDeckNum; i++) {
-				if(nGet == (closedDeck.deckk)[i].nIndex) {
-					break;
+				if(nGet == (closedDeck.deckk)[i].nIndex) { // if random number == index of card in closed deck, then return
+					return nGet;
 				}
 			}
 		}
-		return nGet;
 	}
 	
 	void prepare() {
@@ -43,6 +50,59 @@ public class Gamee {
 			nGet = getCardFromClosed();
 			closedDeck.remove(nGet);
 			p2Hand.append(nGet);
+		}
+	}
+	
+	boolean match(int nMe, int nAnother) {
+		boolean a,b;
+		if(iCard[nMe].sSuite == iCard[nAnother].sSuite) {
+			a = true;
+		} else {
+			a = false;
+		}
+		if(iCard[nMe].sNum == iCard[nAnother].sNum) {
+			b = true;
+		} else {
+			b = false;
+		}
+		return a || b;
+	}
+	
+	void play1(int nMainCard) {
+		
+	}
+	
+	void handPrint(Handd h) { // h : p1Hand or p2Hand
+		for(int i = 0; i < 26; i++) {
+			if(h.handd[i] != null) {
+				cardInfo(h.handd[i]);
+				System.out.printf("%s %s %i", info[0], info[1], infoIndex); //suite number index
+			}
+		}
+	}
+	
+	int play(Handd h, int nMain) { // h : p1Hand or p2Hand, nMain : nMaincard
+		//exception for winning situation (all cards in hand == null)
+		//for all i, if(h.handd[i] == null)
+		int[] LMatch = {0,} ;
+		int n = 0;
+		for(EachCard ec : h.handd) {
+			if(match(ec.nIndex, nMain)) {
+				LMatch[n] = ec.nIndex;
+				n++;
+			}
+		}
+		if(n == 0) { //no match with main card -> draw new card from closed deck
+			nGet = getCardFromClosed();
+			closedDeck.remove(nGet);
+			h.append(nGet);
+			return nGet;
+		} else { // choose 1 from matching cards
+			Random random = new Random();
+			nThrow = LMatch[random.nextInt(n+1)];
+			h.remove(nThrow);
+			closedDeck.append(nThrow);
+			return nThrow;
 		}
 	}
 }
